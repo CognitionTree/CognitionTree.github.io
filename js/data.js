e7707b5edd06a80a995d29fc27684b167c23ce29
andydmh <mhdaniel24@knights.ucf.edu>
2017-04-23 15:46:54 -0400

    Initial page set up

---
+++ js/data.js
@@ -0,0 +1,25 @@
+/*
+    Business Data
+*/
+
+var company_name = "Andy Winery";
+var email_address = "andywinery@gmail.com";
+var phone_number = "786-624-7037";
+
+
+/*
+    Resources
+*/
+
+
+
+/*
+    HTML Properties
+*/
+
+
+
+/*
+    HTML Structures
+*/
+
---
+++ js/main.js
@@ -0,0 +1,3 @@
+//document.getElementById("projects").innerHTML = '<h3 class="margin">PROJECTS</h3> <a href="https://github.com/Learning-With-Purpose/Leaves-Classifier">Leaves Classifier</a>, <a href="https://github.com/Learning-With-Purpose/Deep_ASL_Translator">Deep ASL Translator</a>';
+
+document.write('<h3 class="margin">PROJECTS</h3> <a href="https://github.com/Learning-With-Purpose/Leaves-Classifier">Leaves Classifier</a>, <a href="https://github.com/Learning-With-Purpose/Deep_ASL_Translator">Deep ASL Translator</a>');
